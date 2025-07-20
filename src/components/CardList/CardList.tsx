b3fbb07feb7f15af9a715fc7948b32545e6d6037
sysol8 <iabrosimov@list.ru>
2025-07-20 17:14:24 +0300

    Убрал карту, добавил редактирование и удаление карточек

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,21 +1,29 @@
 import styles from "./CardList.module.css";
 import Card from "../Card/Card.tsx";
 import type { ICar, TCoordinates } from "../../utils/types.ts";
-import CarMap from "../CarMap/CarMap.tsx";
+import CarMap from "../Map/Map.tsx";
+import type { ReactElement } from "react";
+import { useState, useMemo } from "react";
+import Popup from "../Popup/Popup.tsx";
 
 interface CardListProps {
   data: ICar[];
+  hoveredId: string | null;
 }
 
-function CardList({ data }: CardListProps) {
-  const coordinates: TCoordinates = data.map((car) => [
-    car.longitude,
-    car.latitude,
-  ]);
+function CardList({ data, hoveredId }: CardListProps): ReactElement {
+  const [cards, setCards] = useState<ICar[]>(data);
+  const [cardIsEditing, setCardIsEditing] = useState<ICar | null>(null);
+
+  // карточки удаляются только на клиенте и появляются при обновлении страницы, т. к. в задании нет других API-эндпоинтов, кроме GET.
+  // то же актуально и для редактирования карточек
+  const handleCardDelete = (id: string) => {
+    setCards((prev) => prev.filter((card) => card.id !== id));
+  };
 
   return (
-    <main className={`${styles.content} custom-scrollbar`}>
-      {data.map((card) => {
+    <div className={`${styles.cards}`}>
+      {cards.map((card: ICar): ReactElement => {
         return (
           <Card
             key={card.id}
@@ -27,11 +35,33 @@ function CardList({ data }: CardListProps) {
             price={card.price}
             longitude={card.longitude}
             latitude={card.latitude}
+            onDelete={() => handleCardDelete(card.id)}
+            onEdit={() => setCardIsEditing(card)}
+            isHovered={hoveredId === card.id}
           />
         );
       })}
-      <CarMap coordinates={coordinates}></CarMap>
-    </main>
+      {cardIsEditing && (
+        <Popup
+          isOpen={true}
+          fields={[
+            { label: "name", title: "Название" },
+            { label: "price", title: "Цена" },
+          ]}
+          onClose={() => setCardIsEditing(null)}
+          onSubmit={(updatedValues) => {
+            setCards((prev) =>
+              prev.map((card) =>
+                card.id === cardIsEditing.id
+                  ? { ...card, ...updatedValues }
+                  : card,
+              ),
+            );
+            setCardIsEditing(null);
+          }}
+        />
+      )}
+    </div>
   );
 }
 
