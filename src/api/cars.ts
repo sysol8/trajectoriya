4ded9bb80b828b4bc4abb39df96d88f3b43601b3
sysol8 <iabrosimov@list.ru>
2025-07-20 17:14:40 +0300

    Поправил типизацию

--- src/api/cars.ts
+++ src/api/cars.ts
@@ -1,21 +1,21 @@
 import type { ICar } from "../utils/types.ts";
 
-const BASE_URL = 'https://ofc-test-01.tspb.su/test-task/vehicles'
+const BASE_URL = "https://ofc-test-01.tspb.su/test-task/vehicles";
 
 const parseJson = async <T>(response: Response): Promise<T> => {
-    if (!response.ok) {
-        throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
-    }
-    const data = await response.json()
-    return data as T;
-}
+  if (!response.ok) {
+    throw new Error(`Ошибка ${response.status}: ${response.statusText}`);
+  }
+  const data: ICar[] = await response.json();
+  return data as T;
+};
 
 const getCars = async (): Promise<ICar[]> => {
-    const response = await fetch(`${BASE_URL}`, {
-        method: 'GET'
-    })
+  const response: Response = await fetch(`${BASE_URL}`, {
+    method: "GET",
+  });
 
-    return parseJson<ICar[]>(response);
-}
+  return parseJson<ICar[]>(response);
+};
 
-export { getCars }
+export { getCars };
