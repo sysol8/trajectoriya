d343b9f5e9295ef07f369d5c168f166e8122048c
sysol8 <iabrosimov@list.ru>
2025-07-20 17:17:29 +0300

    Теперь карточки выделяются при ховере на элементе списка

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,26 +1,33 @@
-import type {ICar} from '../../utils/types.ts'
-import styles from './Navbar.module.css'
+import type { ICar } from "../../utils/types.ts";
+import styles from "./Navbar.module.css";
 
 interface NavbarProps {
-    data: ICar[]
+  data: ICar[];
+  onHover: (id: string) => void;
+  onLeave: () => void;
 }
 
-function Navbar({ data } : NavbarProps) {
-    return (
-        <nav className={`${styles.nav} custom-scrollbar`}>
-            <ul className={styles.list}>
-                {data.map(item => {
-                    return (
-                        <li key={item.id} className={styles.item}>
-                            <a href={`#${item.id}`} className={styles.link}>
-                                {item.name} {item.model}
-                            </a>
-                        </li>
-                    )
-                })}
-            </ul>
-        </nav>
-    )
+function Navbar({ data, onHover, onLeave }: NavbarProps) {
+  return (
+    <nav className={`${styles.nav} custom-scrollbar`}>
+      <ul className={styles.list}>
+        {data.map((item) => {
+          return (
+            <li
+              key={item.id}
+              className={styles.item}
+              onMouseEnter={() => onHover(item.id)}
+              onMouseLeave={onLeave}
+            >
+              <a href={`#${item.id}`} className={styles.link}>
+                {item.name} {item.model}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
