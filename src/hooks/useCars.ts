988965673558bf39e4f3634f7ea189c9a2f3dad2
sysol8 <iabrosimov@list.ru>
2025-07-20 17:18:23 +0300

    Улучшил типизацию

--- src/hooks/useCars.ts
+++ src/hooks/useCars.ts
@@ -1,33 +1,33 @@
-import { getCars } from '../api/cars.ts'
-import type { ICar } from '../utils/types.ts'
-import { useState, useEffect } from 'react'
+import { getCars } from "../api/cars.ts";
+import type { ICar } from "../utils/types.ts";
+import { useState, useEffect } from "react";
 
-function useCars() {
-    const [cars, setCars] = useState<ICar[]>([]);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState<unknown>(null);
+function useCars(): { cars: ICar[]; loading: boolean; error: unknown } {
+  const [cars, setCars] = useState<ICar[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<unknown>(null);
 
-    useEffect(() => {
-        let isMounted = true;
-        setError(null);
+  useEffect(() => {
+    let isMounted: boolean = true;
+    setError(null);
 
-        (async () => {
-            try {
-                const data: ICar[] = await getCars();
-                if (!isMounted) return;
-                setCars(data);
-            } catch (error) {
-                isMounted && setError(error);
-            } finally {
-                isMounted && setLoading(false);
-            }
-        })();
+    (async () => {
+      try {
+        const data: ICar[] = await getCars();
+        if (!isMounted) return;
+        setCars(data);
+      } catch (error) {
+        isMounted && setError(error);
+      } finally {
+        isMounted && setLoading(false);
+      }
+    })();
 
-        return () => {
-            isMounted = false;
-        };
-    }, [])
-    return { cars, loading, error }
+    return () => {
+      isMounted = false;
+    };
+  }, []);
+  return { cars, loading, error };
 }
 
 export default useCars;
