472ed3a6633aa13af6e8d76b0317393d0ab5cb9e
sysol8 <iabrosimov@list.ru>
2025-07-21 17:54:37 +0300

    Сделал попап с динамически добавляемыми полями

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -1,11 +1,7 @@
 import styles from "./Popup.module.css";
-import { useState } from "react";
-import type { FormEvent } from "react";
-
-interface Field {
-  label: string;
-  title: string;
-}
+import type { FormEvent, ChangeEvent } from "react";
+import type { Field } from "../../utils/types.ts";
+import { useState, useEffect } from "react";
 
 interface PopupProps {
   fields: Field[];
@@ -17,39 +13,70 @@ interface PopupProps {
 function Popup({ fields, isOpen, onClose, onSubmit }: PopupProps) {
   const [formValues, setFormValues] = useState<Record<string, string>>({});
 
-  const handleChange = (label: string, value: string) => {
-    setFormValues((prev) => ({ ...prev, [label]: value }));
-  };
+  useEffect(() => {
+    if (!isOpen) return;
+    const initialValues: Record<string, string> = {};
+    for (const { label, value } of fields) {
+      initialValues[label] = String(value);
+    }
+    setFormValues(initialValues);
+  }, [fields, isOpen]);
 
-  const handleSubmit = (e: FormEvent) => {
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     onSubmit(formValues);
     onClose();
   };
 
+  const handleInputChange = (label: string, value: string) => {
+    setFormValues((prev) => ({ ...prev, [label]: value }));
+  };
+
+  const handleCloseByEsc = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      onClose();
+    }
+  };
+
+  const handleCloseByOverlayClick = (e: MouseEvent) => {
+    if ((e.target as HTMLElement).classList.contains(styles.backdrop)) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    if (!isOpen) return;
+
+    document.addEventListener("keydown", handleCloseByEsc);
+    document.addEventListener("click", handleCloseByOverlayClick);
+
+    return () => {
+      document.removeEventListener("keydown", handleCloseByEsc);
+      document.removeEventListener("click", handleCloseByOverlayClick);
+    };
+  }, [isOpen]);
+
   return (
     <div className={`${styles.backdrop} ${isOpen ? styles.popupOpen : ""}`}>
-      <form className={`${styles.popup}`} onSubmit={handleSubmit}>
-        <h3>Редактировать</h3>
-        <button
-          className={styles.close}
-          onClick={(e) => {
-            e.preventDefault();
-            onClose();
-          }}
-        >
+      <form className={`${styles.popup}`} onSubmit={handleFormSubmit}>
+        <h3 className={styles.heading}>Редактировать</h3>
+        <button className={styles.close} type="button" onClick={onClose}>
           x
         </button>
-        {fields.map(({ label, title }) => (
-          <label key={label}>
-            {title}
-            <input
-              className={styles.input}
-              value={formValues[label] ?? ""}
-              onChange={(e) => handleChange(label, e.target.value)}
-            />
-          </label>
-        ))}
+        <fieldset className={styles.fieldset}>
+          {fields.map(({ label, title }) => (
+            <label key={label} className={styles.label}>
+              {title}
+              <input
+                className={styles.input}
+                value={formValues[label] ?? ""}
+                onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                  handleInputChange(label, e.target.value)
+                }
+              />
+            </label>
+          ))}
+        </fieldset>
         <button className={styles.submit} type="submit">
           Сохранить
         </button>
