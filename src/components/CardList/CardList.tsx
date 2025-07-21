69e749130ab45bf3b0ba3c5ec302422056938513
sysol8 <iabrosimov@list.ru>
2025-07-21 17:52:17 +0300

    Перенес карту в лейаут

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,10 +1,8 @@
 import styles from "./CardList.module.css";
 import Card from "../Card/Card.tsx";
-import type { ICar, TCoordinates } from "../../utils/types.ts";
-import CarMap from "../Map/Map.tsx";
+import type { ICar } from "../../utils/types.ts";
 import type { ReactElement } from "react";
-import { useState, useMemo } from "react";
-import Popup from "../Popup/Popup.tsx";
+import { useState } from "react";
 
 interface CardListProps {
   data: ICar[];
@@ -13,7 +11,6 @@ interface CardListProps {
 
 function CardList({ data, hoveredId }: CardListProps): ReactElement {
   const [cards, setCards] = useState<ICar[]>(data);
-  const [cardIsEditing, setCardIsEditing] = useState<ICar | null>(null);
 
   // карточки удаляются только на клиенте и появляются при обновлении страницы, т. к. в задании нет других API-эндпоинтов, кроме GET.
   // то же актуально и для редактирования карточек
@@ -36,31 +33,10 @@ function CardList({ data, hoveredId }: CardListProps): ReactElement {
             longitude={card.longitude}
             latitude={card.latitude}
             onDelete={() => handleCardDelete(card.id)}
-            onEdit={() => setCardIsEditing(card)}
             isHovered={hoveredId === card.id}
           />
         );
       })}
-      {cardIsEditing && (
-        <Popup
-          isOpen={true}
-          fields={[
-            { label: "name", title: "Название" },
-            { label: "price", title: "Цена" },
-          ]}
-          onClose={() => setCardIsEditing(null)}
-          onSubmit={(updatedValues) => {
-            setCards((prev) =>
-              prev.map((card) =>
-                card.id === cardIsEditing.id
-                  ? { ...card, ...updatedValues }
-                  : card,
-              ),
-            );
-            setCardIsEditing(null);
-          }}
-        />
-      )}
     </div>
   );
 }
