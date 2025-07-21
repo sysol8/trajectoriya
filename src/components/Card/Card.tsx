5344433602bdd05f53a561eba2d40edbe1cd9f0e
sysol8 <iabrosimov@list.ru>
2025-07-21 17:51:40 +0300

    Добавил редактирование карточки

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,10 +1,11 @@
 import styles from "./Card.module.css";
-import type { ICar } from "../../utils/types.ts";
+import Popup from "../Popup/Popup.tsx";
+import type { ICar, Field } from "../../utils/types.ts";
+import { useState } from "react";
 // @ts-ignore
 import DeleteIcon from "../../assets/icons/delete.svg?react";
 // @ts-ignore
 import EditIcon from "../../assets/icons/edit.svg?react";
-/*import type { ChangeEvent, FormEvent, FormEventHandler } from "react";*/
 
 interface ICardProps extends ICar {
   onEdit: () => void;
@@ -12,52 +13,82 @@ interface ICardProps extends ICar {
   isHovered: boolean;
 }
 
-type editable = boolean;
-
 function Card({
   id,
-  name,
+  name: initialName,
   model,
   year,
   color,
-  price,
+  price: initialPrice,
   latitude,
   longitude,
-  onEdit,
   onDelete,
   isHovered,
 }: ICardProps) {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [name, setName] = useState(initialName);
+  const [price, setPrice] = useState(initialPrice);
+
+  const editableFields: Field[] = [
+    {
+      label: "name",
+      title: "Название",
+      value: name,
+    },
+    {
+      label: "price",
+      title: "Цена",
+      value: price,
+    },
+  ];
+
+  const handleSave = (values: Record<string, string>) => {
+    setName(values.name);
+    setPrice(Number(values.price));
+    setIsEditing(false);
+  };
+
   return (
-    <article
-      className={`${styles.card} ${isHovered ? styles.cardHovered : ""}`}
-      id={id}
-    >
-      <div className={styles.text}>
-        <h2 className={styles.heading}>
-          {name} {model}
-        </h2>
-        <p className={styles.textContent}>Год выпуска: {year.toString()}</p>
-        <p className={styles.textContent}>Цена: {price}</p>
-        <p className={styles.textContent}>
-          Координаты: {longitude}, {latitude}
-        </p>
-      </div>
-      <div className={styles.colorContainer}>
-        <span>Цвет</span>
-        <span
-          className={styles.color}
-          style={{ backgroundColor: color }}
-        ></span>
-      </div>
-      <div className={styles.buttons}>
-        <button className={styles.button} onClick={onEdit}>
-          <EditIcon className={styles.icon} />
-        </button>
-        <button className={styles.button} onClick={onDelete}>
-          <DeleteIcon className={styles.icon} />
-        </button>
-      </div>
-    </article>
+    <>
+      <article
+        className={`${styles.card} ${isHovered ? styles.cardHovered : ""}`}
+        id={id}
+      >
+        <div className={styles.text}>
+          <h2 className={styles.heading}>
+            {name} {model}
+          </h2>
+          <p className={styles.textContent}>Год выпуска: {year}</p>
+          <p className={styles.textContent}>Цена: {price}</p>
+          <p className={styles.textContent}>
+            Координаты: {longitude}, {latitude}
+          </p>
+        </div>
+        <div className={styles.colorContainer}>
+          <span>Цвет</span>
+          <span
+            className={styles.color}
+            style={{ backgroundColor: color }}
+          ></span>
+        </div>
+        <div className={styles.buttons}>
+          <button className={styles.button} onClick={() => setIsEditing(true)}>
+            <EditIcon className={styles.icon} />
+          </button>
+          <button className={styles.button} onClick={onDelete}>
+            <DeleteIcon className={styles.icon} />
+          </button>
+        </div>
+      </article>
+      {isEditing && (
+        <Popup
+          fields={editableFields}
+          isOpen={isEditing}
+          onClose={() => setIsEditing(!isEditing)}
+          onSubmit={handleSave}
+        ></Popup>
+      )}
+    </>
   );
 }
 
