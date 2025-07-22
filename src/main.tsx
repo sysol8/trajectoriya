ad7146d20c5516965ea23d738f150f40be9e90e3
sysol8 <iabrosimov@list.ru>
2025-07-22 16:51:04 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import './styles/variables.css';
-import './styles/global.css';
-import './styles/style.css';
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/style.css";
 //@ts-ignore
-import '@fontsource-variable/arimo'
+import "@fontsource-variable/arimo";
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import CarsLayout from "./layouts/CarsLayout.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CarsLayout />
   </StrictMode>,
-)
+);
