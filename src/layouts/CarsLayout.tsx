a155575315681bc377d88d37fbce832e208496d7
sysol8 <iabrosimov@list.ru>
2025-10-08 17:34:51 +0300

    feat: оптимизировал, добавил отдельный компонент с контролами, избавился от prop drilling

--- src/layouts/CarsLayout.tsx
+++ src/layouts/CarsLayout.tsx
@@ -1,41 +1,46 @@
 import styles from "./CarsLayout.module.css";
-import type { MapMarker } from "../utils/types.ts";
-import { useState } from "react";
-import useCars from "../hooks/useCars.ts";
+import { useEffect, useState, useCallback } from "react";
 import Navbar from "../components/Navbar/Navbar.tsx";
 import CardList from "../components/CardList/CardList.tsx";
-import CarMap from "../components/Map/Map.tsx";
+import Header from "../components/Header/Header.tsx";
+import { useBoundStore } from "../store/store.ts";
+import CarMap from '../components/Map/Map.tsx';
 
 function CarsLayout() {
-  const { cars, loading, error } = useCars();
+  const getCars = useBoundStore((state) => state.getCars);
   const [hoveredId, setHoveredId] = useState<string | null>(null);
 
-  const carMarkers: MapMarker[] = cars.map((car) => ({
-    textContent: `${car.name} ${car.model}`,
-    coordinates: [car.longitude, car.latitude],
-  }));
+  const handleMenuItemHover = useCallback((id: string) => {
+    setHoveredId(id)
+  }, [])
 
-  if (loading) return <p>Загрузка...</p>;
-  if (error) return <p>Ошибка</p>;
+  const handleMenuItemLeave = useCallback(() => {
+    setHoveredId(null);
+  }, [])
+
+  useEffect(() => {
+    getCars();
+  }, []);
 
   return (
     <div className={styles.layout}>
       <Navbar
-        data={cars}
-        onHover={setHoveredId}
-        onLeave={() => setHoveredId(null)}
+        onHover={handleMenuItemHover}
+        onLeave={handleMenuItemLeave}
       ></Navbar>
-      <main className={`${styles.main} custom-scrollbar`}>
-        <h1>Автомобили</h1>
-        <section>
-          <h2 className="visually-hidden">Список автомобилей</h2>
-          <CardList data={cars} hoveredId={hoveredId}></CardList>
-        </section>
-        <section>
-          <h2 className={styles.subheading}>Автомобили на карте</h2>
-          <CarMap markers={carMarkers}></CarMap>
-        </section>
-      </main>
+      <div className={styles.content}>
+        <Header title={"Автомобили"} />
+        <main className={`${styles.main} custom-scrollbar`}>
+          <section>
+            <h2 className="visually-hidden">Список автомобилей</h2>
+            <CardList hoveredId={hoveredId}></CardList>
+          </section>
+          <section>
+            <h2 className={styles.subheading}>Автомобили на карте</h2>
+            <CarMap />
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
