23c67e59c2db35e74b954052df1b620a338756db
sysol8 <iabrosimov@list.ru>
2025-10-08 17:32:02 +0300

    chore: вынес App в отдельный компонент

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import CarsLayout from "./layouts/CarsLayout.tsx";
+import Modal from "./components/Modal/Modal.tsx";
+
+function App() {
+  return (
+    <>
+      <CarsLayout />
+      <Modal />
+    </>
+  )
+}
+
+export default App;
