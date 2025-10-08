789cea497b013c874bb1bb15d6894b47e3c864a5
sysol8 <iabrosimov@list.ru>
2025-10-08 19:28:04 +0300

    feat: добавил компонент-фолбек для обработки ошибки и загрузки

--- src/layouts/CarsLayout.tsx
+++ src/layouts/CarsLayout.tsx
@@ -4,23 +4,29 @@ import Navbar from "../components/Navbar/Navbar.tsx";
 import CardList from "../components/CardList/CardList.tsx";
 import Header from "../components/Header/Header.tsx";
 import { useBoundStore } from "../store/store.ts";
-import CarMap from '../components/Map/Map.tsx';
+import CarMap from "../components/Map/Map.tsx";
 
 function CarsLayout() {
   const getCars = useBoundStore((state) => state.getCars);
+  const loading = useBoundStore((state) => state.loading);
+  const error = useBoundStore((state) => state.error);
   const [hoveredId, setHoveredId] = useState<string | null>(null);
 
   const handleMenuItemHover = useCallback((id: string) => {
-    setHoveredId(id)
-  }, [])
+    setHoveredId(id);
+  }, []);
 
   const handleMenuItemLeave = useCallback(() => {
     setHoveredId(null);
-  }, [])
+  }, []);
 
   useEffect(() => {
     getCars();
   }, []);
+
+  if (loading) return <Fallback error={null} loading={loading} />;
+
+  if (error) return <Fallback error={error} loading={loading} />;
 
   return (
     <div className={styles.layout}>
@@ -43,6 +49,29 @@ function CarsLayout() {
       </div>
     </div>
   );
+}
+
+interface FallbackProps {
+  error: string | null;
+  loading: boolean;
+}
+
+function Fallback({ error, loading }: FallbackProps) {
+  if (error)
+    return (
+      <div className={styles.layout}>
+        <p className={styles.fallback}>{error}</p>
+      </div>
+    );
+
+  if (loading)
+    return (
+      <div className={styles.layout}>
+        <p className={styles.fallback}>Загрузка</p>
+      </div>
+    );
+
+  return null;
 }
 
 export default CarsLayout;
