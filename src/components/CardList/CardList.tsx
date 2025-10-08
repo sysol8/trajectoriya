d1e7098176079649690c3487d713abd3d5a83f33
sysol8 <iabrosimov@list.ru>
2025-10-08 17:33:00 +0300

    feat: интегрировал стор, переделал сортировку

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,78 +1,46 @@
-// CardList.tsx
 import styles from "./CardList.module.css";
 import type { ICar } from "../../utils/types.ts";
-import type { ReactElement } from "react";
-import { useState, useMemo } from "react";
+import { type ReactElement, useMemo } from "react";
 import Card from "../Card/Card.tsx";
-
-type SortKey = "year" | "price" | "";
-type SortOrder = "asc" | "desc";
+import { useBoundStore } from "../../store/store.ts";
 
 interface CardListProps {
-  data: ICar[];
   hoveredId: string | null;
 }
 
-function CardList({ data, hoveredId }: CardListProps): ReactElement {
-  const [cards, setCards] = useState<ICar[]>(data);
-  const [sortKey, setSortKey] = useState<SortKey>("");
-  const [sortOrder, setSortOrder] = useState<SortOrder>("asc");
+function CardList({ hoveredId }: CardListProps): ReactElement {
+  // Здесь сортируем карточки и мемоизируем результат. Эту логику можно вынести в кастомный хук, чтобы переиспользовать, например, в навигационной панели.
+  // В данном случае сортировку оставляем только в списке карточек.
+  const cars = useBoundStore((state) => state.cars);
+  const sortField = useBoundStore((state) => state.sortField);
+  const sortOrder = useBoundStore((state) => state.sortOrder);
 
-  const handleCardDelete = (id: string) => {
-    setCards((prev) => prev.filter((card) => card.id !== id));
-  };
-
-  const sortedCards = useMemo(() => {
-    if (!sortKey) return cards;
-    return [...cards].sort((a, b) => {
-      const diff = a[sortKey] - b[sortKey];
-      return sortOrder === "asc" ? diff : -diff;
+  const sortedCars = useMemo(() => {
+    if (!sortField) return cars;
+    return [...cars].sort((a, b) => {
+      const diff =
+        (a[sortField] as unknown as number) -
+        (b[sortField] as unknown as number);
+      return sortOrder === 'asc' ? diff : -diff;
     });
-  }, [cards, sortKey, sortOrder]);
+  }, [cars, sortOrder, sortField]);
 
   return (
     <>
-      <div className={styles.sortControls}>
-        <label>
-          Сортировать по:&nbsp;
-          <select className={styles.select}
-            value={sortKey}
-            onChange={(e) => setSortKey(e.target.value as SortKey)}
-          >
-            <option className={styles.option} value="">—</option>
-            <option className={styles.option} value="year">Год</option>
-            <option className={styles.option} value="price">Цена</option>
-          </select>
-        </label>
-
-        <label>
-          Порядок:&nbsp;
-          <select className={styles.select}
-            value={sortOrder}
-            onChange={(e) => setSortOrder(e.target.value as SortOrder)}
-            disabled={!sortKey}
-          >
-            <option className={styles.option} value="asc">По возрастанию</option>
-            <option className={styles.option} value="desc">По убыванию</option>
-          </select>
-        </label>
-      </div>
-
       <div className={styles.cards}>
-        {sortedCards.map(
-          (card: ICar): ReactElement => (
+        {sortedCars.map(
+          (car: ICar): ReactElement => (
             <Card
-              key={card.id}
-              id={card.id}
-              name={card.name}
-              model={card.model}
-              year={card.year}
-              color={card.color}
-              price={card.price}
-              longitude={card.longitude}
-              latitude={card.latitude}
-              onDelete={() => handleCardDelete(card.id)}
-              isHovered={hoveredId === card.id}
+              key={car.id}
+              id={car.id}
+              name={car.name}
+              model={car.model}
+              year={car.year}
+              color={car.color}
+              price={car.price}
+              longitude={car.longitude}
+              latitude={car.latitude}
+              isHovered={hoveredId === car.id}
             />
           ),
         )}
